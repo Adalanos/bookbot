f5dc0122b563f0e428ce8ea580ed3af252ac34bb
Adalanos <bolanosadalberto@outlook.com>
2026-01-06 15:44:10 -0800

    main counts words in a book

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def count_book_words(book_text):
+    return len(book_text.split())
+
+def get_book_text(path_to_file):
+    file_contents = None
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def main():
+    print(f'Found {count_book_words(get_book_text("books/frankenstein.txt"))} total words')
+pass
+
+main()
