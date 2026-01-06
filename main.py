2fdfc2c2a17704a4263809167369b7fe2242b2b9
Adalanos <bolanosadalberto@outlook.com>
2026-01-06 15:57:52 -0800

    added gitignore and refactored main with stats

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-def count_book_words(book_text):
-    return len(book_text.split())
+from stats import count_book_words
 
 def get_book_text(path_to_file):
     file_contents = None
+
     with open(path_to_file) as f:
         file_contents = f.read()
-    
+
     return file_contents
 
 def main():
-    print(f'Found {count_book_words(get_book_text("books/frankenstein.txt"))} total words')
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = count_book_words(book_text)
+
+    print(f'Found {word_count} total words')
 pass
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def count_book_words(book_text):
+    book_words_list = book_text.split()
+    word_count = len(book_words_list)
+    
+    return word_count
